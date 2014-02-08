eaede888153068714ebea2a20b5155d3e04dc97b
Marc Tamlyn <marc.tamlyn@gmail.com>
2014-02-08 15:19:05 +0000

    Set my site url correctly.

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Marc Tamlyn'
 SITENAME = 'Improved PostgreSQL support for Django'
-SITEURL = ''
+SITEURL = 'http://postgres.mjtamlyn.co.uk/'
 
 TIMEZONE = 'Europe/London'
 
